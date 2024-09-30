c879c772af5007ab04db5494a7814b756c888fb2
shamirul <hudashamirul@gmail.com>
2024-09-30 22:37:52 +0530

    first row of buttons done

--- script.js
+++ script.js
@@ -14,5 +14,69 @@ function divide(x,y){
     return x/y;
 }
 
-const btn = document.querySelector("#buttons");
+const ctner = document.querySelector("#ctner");
 
+ctner.setAttribute("style",
+    "border:1px solid cyan;"+
+    "height:75%;"+
+    "display:flex;"+
+    "flex-direction:column;");
+
+let div = document.createElement("div");
+div.setAttribute("style","height:20%;width:100%;display:flex;justify-content:space-evenly;border:3px solid blue;");
+div.setAttribute("class","row1");
+ctner.appendChild(div);
+
+const row1 = document.querySelector(".row1");
+
+for(let i=0;i<2;i++)
+{
+    let div = document.createElement("div");
+    div.setAttribute("style","height:100%;width:50%;border:2px solid yellow;display:flex;justify-content:space-around;align-items:center;");
+    i==0?div.setAttribute("class","special"):div.setAttribute("class","operations");
+
+    row1.appendChild(div);
+}
+
+const special = document.querySelector(".special");
+for(let i=0;i<2;i++)
+{
+    let button = document.createElement("button");
+    button.setAttribute("style",
+        "background-color:#cd3300;"+
+        "padding:32px;"+
+        "height:50px;"+
+        "border-radius:8px;"+
+        "color:white;"+
+        "font-weight:bolder;"+
+        "text-align:center;"+
+        "line-height:10%;");
+
+    i==0? button.textContent="AC":button.textContent="C";
+    special.appendChild(button);
+}
+
+const operations = document.querySelector(".operations");
+for(let i=0;i<2;i++)
+{
+    let button = document.createElement("button");
+    button.setAttribute("style",
+        "background-color:#ffa53f;"+
+        "padding:32px;"+
+        "height:50px;"+
+        "border-radius:8px;"+
+        "color:white;"+
+        "font-weight:bolder;"+
+        "text-align:center;"+
+        "line-height:10%;");
+
+    i==0? button.textContent="+/-":button.textContent=" * ";
+    operations.appendChild(button);
+}
+
+let div2 = document.createElement("div");
+div2.setAttribute("style","height:20%;width:100%;display:flex;justify-content:space-evenly;border:3px solid blue;");
+div2.setAttribute("class","row2");
+ctner.appendChild(div2);
+
+const row2 = document.querySelector(".row2");
